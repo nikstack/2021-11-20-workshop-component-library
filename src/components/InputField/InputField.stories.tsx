68fd89eb7ce472b247682f415bb02a5a19ba98c7
niklas <nskf2000@gmail.com>
2021-11-20 14:11:23 +0100

    structure for formatter

--- src/components/InputField/InputField.stories.tsx
+++ src/components/InputField/InputField.stories.tsx
@@ -4,14 +4,17 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { InputField } from "./InputField";
 
 export default {
-  title: "Components/InputField",
-  component: InputField,
-  argTypes: {},
+    title: "Components/InputField",
+    component: InputField,
+    argTypes: {},
 } as ComponentMeta<typeof InputField>;
 
 const Template: ComponentStory<typeof InputField> = (args) => (
-  <InputField {...args} />
+    <InputField {...args} />
 );
 
 export const Default = Template.bind({});
 Default.args = {};
+
+export const WithPlaceholderAndValue = Template.bind({});
+WithPlaceholderAndValue.args = { placeholder: 'Hello World', defaultValue: 'Default Value' }
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "./InputField.css";
 
-interface InputFieldProps {}
+export enum FormatType {
+    TELEPHONE = 'TELEPHONE',
+    IBAN = 'IBAN',
+}
 
-export const InputField = ({ ...props }: InputFieldProps) => {
-  // --- YOUR CODE HERE ---
+interface InputFieldProps extends HTMLInputElement {
+    formatType?: FormatType,
+}
 
-  return <input placeholder='Hello World' />;
+export const InputField = ({ formatType, defaultValue, placeholder }: InputFieldProps) => {
+    // --- YOUR CODE HERE ---
+
+    const [value, setValue] = useState<string>(defaultValue);
+
+    const getFormatter = () => {
+        if (!formatType) {
+            return;
+        }
+        switch (formatType) {
+            case FormatType.TELEPHONE:
+                return new TelephoneFormatter();
+            case FormatType.IBAN:
+                return new IBANFormatter();
+            default:
+                return null;
+        }
+    }
+
+    const formatter = getFormatter();
+
+    const format = (value: string) => {
+        if (!formatter) {
+            return;
+        }
+
+        setValue(() => (formatter.getFormattedValue(value)))
+    }
+
+    return <input placeholder={placeholder}
+                  defaultValue={defaultValue}
+                  onChange={e => {
+                      format(e.target.value)
+                  }}
+    />;
 };
+
+abstract class Formatter {
+    getFormattedValue(value: string): string {
+        return ''
+    }
+
+    constructor(private regex: RegExp) {
+    }
+
+}
+
+class IBANFormatter extends Formatter {
+    constructor() {
+        super(/\w\w\d\d/);
+    }
+}
+
+class TelephoneFormatter extends Formatter {
+    constructor() {
+        super(/\w\w\d\d/);
+    }
+}
