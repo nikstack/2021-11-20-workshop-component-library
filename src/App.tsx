fa62a85700a971edeb1521c0feabbea8cb895604
Jonas Deipenbrock <jonas.deipenbrock@gmx.de>
2021-11-20 14:16:03 +0100

    Adds select functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import "./App.css";
 import React from "react";
 
 import { ApplicationPage } from "./pages/Application/ApplicationPage";
+import { Select } from "./components/Select/Select";
 
 export const App = () => (
-  <div className="App">
-    <ApplicationPage />
-  </div>
+    <div className="App">
+        {/* <ApplicationPage /> */}
+        <Select options={["Option1", "Option2"]} />
+    </div>
 );
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,10 +1,67 @@
 import React from "react";
 import "./Select.css";
 
-interface SelectProps {}
+interface SelectProps {
+    selectedOption?: string;
+    options: string[];
+    label?: string;
+    placeholder?: string;
+}
 
-export const Select = ({ ...props }: SelectProps) => {
-  // --- YOUR CODE HERE ---
+export const Select = (props: SelectProps) => {
+    const [selected, setSelected] = React.useState<string>();
+    const [open, toggleOpen] = React.useState(false);
 
-  return <>Select</>;
+    React.useEffect(() => {
+        if (!props.selectedOption) return;
+        setSelected(props.selectedOption);
+    }, [props.selectedOption]);
+
+    const onSelect = (option: string) => {
+        toggleOpen(false);
+        setSelected(option);
+        console.log("fired");
+    };
+
+    const toggleDropdown = () => {
+        toggleOpen(!open);
+    };
+
+    return (
+        <>
+            {props.label && <text id="select-label">{props.label}</text>}
+            <div id="select-selected">
+                {selected ? (
+                    <button
+                        value=""
+                        onClick={toggleDropdown}
+                        className="select-button">
+                        {selected}
+                    </button>
+                ) : (
+                    <button
+                        value=""
+                        onClick={toggleDropdown}
+                        id="select-placeholder"
+                        className="select-button">
+                        {props.placeholder ?? "placeholder"}
+                    </button>
+                )}
+            </div>
+            {open && (
+                <div id="select-dropdown">
+                    {props.options.map((option) => {
+                        return (
+                            <button
+                                key={option}
+                                onClick={() => onSelect(option)}
+                                className="select-button select-dropdown-button">
+                                {option}
+                            </button>
+                        );
+                    })}
+                </div>
+            )}
+        </>
+    );
 };
