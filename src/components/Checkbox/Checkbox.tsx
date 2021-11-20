ddf298b4ccca6910ac04eb1dc6d998ecb054f966
Stremlau <tilman.stremlau@campusjaeger.de>
2021-11-20 15:13:54 +0100

    add checkbox component

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Checkbox.css";
+import { ReactComponent as CheckIcon } from './check.svg';
 
-interface CheckboxProps {}
+interface CheckboxProps {
+    name: string;
+    value: string;
+    label: string;
+    isInitialChecked: boolean;
+    handleChanged: (isChecked: boolean) => void;
+}
 
-export const Checkbox = ({ ...props }: CheckboxProps) => {
-  // --- YOUR CODE HERE ---
+export const Checkbox = ({...props}: CheckboxProps) => {
 
-  return <>Checkbox</>;
+    const [isChecked, setIsChecked] = useState(props.isInitialChecked);
+
+    function toggle(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        const toggledIsChecked = !isChecked;
+
+        setIsChecked(toggledIsChecked);
+        props.handleChanged && props.handleChanged(toggledIsChecked);
+    }
+
+    return <>
+        <div className={`Checkbox ${isChecked ? 'checked' : ''}`}>
+            <label onClick={toggle}>
+                <input type="checkbox" name={props.name} value={props.name} onChange={toggle}/>
+                <div className="box">{isChecked ? <CheckIcon/> : '\u00A0'}</div>
+                {' '}
+                {props.label}
+            </label>
+        </div>
+    </>;
 };
